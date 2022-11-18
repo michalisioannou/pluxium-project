84154cea39cb1fff0322f795def05ee8034aac6d
alexb <baocean@gmail.com>
2022-11-18 16:03:26 +0200

    allclients response fixed

--- backend/src/read-all-clients.route.ts
+++ backend/src/read-all-clients.route.ts
@@ -2,5 +2,5 @@ import {db} from './database'
 
 export function readAllClients(req, res) {
   console.log("user is reading all clients");
-  res.status(200).json({lessons:db.readAllLessons()});
+  res.status(200).json({clients:db.readAllLessons()});
 }
